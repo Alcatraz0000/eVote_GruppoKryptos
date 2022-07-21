3631765591aa17dd89c7d59e0fe98b9b0dee9ad4
alcatraz <ferdisica@icloud.com>
2022-07-21 12:52:14 +0200

    ClassiDefinitive

--- eVote/src/evote/EVote.java
+++ eVote/src/evote/EVote.java
@@ -35,23 +35,6 @@ public class EVote {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        String name = "secp256k1"; // type of elliptic curve, other examples secp256k1
-        KeyPairGenerator key = KeyPairGenerator.getInstance("ECDSA");
-        key.initialize(new ECGenParameterSpec(name));
-        KeyPairGenerator keyMD = KeyPairGenerator.getInstance("ECDSA");
-        keyMD.initialize(new ECGenParameterSpec(name));
-        KeyPair keyPair = key.generateKeyPair();
-        KeyPair keyPairMD = keyMD.generateKeyPair();
-        // you can use this pattern to encrypt concatenation of several objects
-        byte[] plainTextPKB = keyPair.getPublic().getEncoded(); // this is the public key to encrypt
-        byte[] plainTextSig = "Signature".getBytes(); // here the plaintext is a string but could be
-        // a byte array containing a real signature
-        byte[] plainTextPreimage = "r1".getBytes(); // this could be the message signed in the signature
-        //ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        
-        System.out.print(keyPair.getPublic() + "  "  + keyPairMD.getPrivate() );
-                                                    
-                                                    
                                                     
     }
     
---
+++ eVote/src/evote/SmartContract.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package evote;
+
+/**
+ *
+ * @author nando
+ * @time Jul 21, 2022 12:51:38 PM
+ */
+public class SmartContract {
+
+}
